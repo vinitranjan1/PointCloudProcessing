99580e00ceacc933b175fa66fb9e8b2925d8cdf5
Vinit Ranjan <Vinit@Vinits-MacBook-Pro.local>
2018-06-26 11:17:59 -0700

    no vis fix

--- NoVisuals/NoVisualANNGuidedFilter.py
+++ NoVisuals/NoVisualANNGuidedFilter.py
@@ -2,7 +2,6 @@ from Filters.ANNGuidedFilter import ann_guided_filter
 from tqdm import tqdm, trange
 import numpy as np
 from ReadRawLAS import read_raw_las_data
-from VtkPointCloud import VtkPointCloud
 from laspy.file import File
 from PointCloudPlotQt import create_point_cloud_plot_qt
 import pdb
@@ -10,15 +9,14 @@ import pdb
 input1 = "../MantecaDock/fourPallets.las"
 output1 = "Output/temp.las"
 
-pc = VtkPointCloud()
-pc2 = VtkPointCloud()
+pc = np.array([], dtype=np.float32)
+pc2 = np.array([], dtype=np.float32)
 with File(input1, mode='r') as f:
     input_header = f.header
 
     raw_points = read_raw_las_data(input1)
     points = ann_guided_filter(raw_points, neighbors=50, filter_eps=.07)
-    for point in tqdm(points, total=len(points), desc="Adding to pc"):
-        pc.addPoint(point)
+
     #
     #
     # points2 = read_raw_las_data(output1)
@@ -30,14 +28,14 @@ with File(input1, mode='r') as f:
 
     with File(output1, mode='w', header=input_header) as file:
         # pdb.set_trace()
-        points = pc.getPoints()
+        # points = pc.getPoints()
         # print(points)
         allx = []
         ally = []
         allz = []
         # for p in tqdm(points, total=points.GetNumberOfPoints(), desc="Saving"):
-        for i in tqdm(range(points.GetNumberOfPoints()), total=points.GetNumberOfPoints(), desc="Saving"):
-            p = points.GetPoint(i)
+        for i in trange(len(points), desc="Saving"):
+            p = points[i]
             # print(p)
             allx.append(p[0])
             ally.append(p[1])

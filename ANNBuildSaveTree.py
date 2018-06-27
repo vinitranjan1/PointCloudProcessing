6bbc911bb1a800f0e9a76f4584fdc4fc52b97417
Vinit Ranjan <Vinit@Vinits-MacBook-Pro.local>
2018-06-27 11:10:25 -0700

    save ann tree

---
+++ ANNBuildSaveTree.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pdb
+from scipy.spatial import kdtree
+from ReadRawLAS import read_raw_las_data
+import time
+from tqdm import tqdm, trange
+from annoy import AnnoyIndex
+
+
+input1 = "MantecaDock/room1.las"
+
+
+def ann_build_save_tree(input_cloud, tree_name, dim=3):
+    num = len(input_cloud)
+    tree = AnnoyIndex(dim, metric='euclidean')
+    for k in trange(num, desc="Preparing for ANN"):
+        tree.add_item(k, input_cloud[k])
+
+    start = time.time()
+    num_trees = 5
+    tree.build(num_trees)
+    end = time.time() - start
+    print("Building %d trees took %d seconds" % (num_trees, end))
+    tree.save(tree_name)
+    print("Tree saved")
+
+
+ann_build_save_tree(read_raw_las_data(input1), "MantecaDock/room1.tree")
--- SubsampleSave.py
+++ SubsampleSave.py
@@ -3,9 +3,9 @@ from SubsampleFrac import subsample_frac
 from ReadRawLAS import read_raw_las_data
 from laspy.file import File
 
-input1 = "../Data/room1.las"
-out = "../Data/room1p1samp.las"
+input1 = "MantecaRoom1/room1.las"
+out = "MantecaRoom1/room1p5samp.las"
 with File(input1, mode='r') as file:
     points = read_raw_las_data(input1)
-    samp = subsample_frac(points, .1)
+    samp = subsample_frac(points, .5)
     save_points_as_las(samp, out, file.header)
